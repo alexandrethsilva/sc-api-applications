3b78d68948eacb9b902c81c6172fcd5dc3a1511d
Alexandre Theodoro da Silva <alexandrethsilva@mac.com>
2017-08-01 11:32:58 +0200

    Updates messages

--- src/constants/index.js
+++ src/constants/index.js
@@ -5,7 +5,9 @@ module.exports.API_DEFAULT_DOMAIN = "api.spotcap.com"
 module.exports.SESSION_KEY = "sc_applications_sid"
 module.exports.SESSION_SECRET = "ponytails"
 
-module.exports.MESSAGE_DEFAULT_LOGGED_IN = {message: "Heres ya cookiez!"}
-module.exports.MESSAGE_DEFAULT_LOGGED_OUT = {message: "Bye bye cookiez!"}
+module.exports.MESSAGE_DEFAULT_LOGGED_IN = {message: "Your session was initiated."}
+module.exports.MESSAGE_DEFAULT_LOGGED_OUT = {message: "Your session was terminated."}
 
-module.exports.MESSAGE_DEFAULT_UNAUTHORIZED = {message: "Nope. Go away!"}
+module.exports.MESSAGE_DEFAULT_UNAUTHORIZED = {
+  error: "Access unauthorized. There is no active session.",
+}
